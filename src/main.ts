c819f5dd840b5201fc7f2e1c769ec7ff4d8cead0
jackykwandesign <jackykwandesign@gmail.com>
2022-08-19 13:50:44 +0000

    remove dotnet require

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 
-require("dotenv").config()
+import * as dotenv from "dotenv"
+dotenv.config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   app.use(bodyParser.json({limit: '50mb'}));
   app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
