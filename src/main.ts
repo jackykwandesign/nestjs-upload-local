db42e46849c9c4a15cc463f29b85142836661e7b
jackykwandesign <jackykwandesign@gmail.com>
2022-11-20 09:55:16 +0000

    chore: auto format

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as bodyParser from 'body-parser';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import * as bodyParser from 'body-parser'
 
-import * as dotenv from "dotenv"
-import { initStoragePath } from './utils/initStoragePath';
+import * as dotenv from 'dotenv'
+import { initStoragePath } from './utils/initStoragePath'
 
 dotenv.config()
 
 async function bootstrap() {
+    // check file path if exist
+    initStoragePath()
 
-  // check file path if exist
-  initStoragePath()
+    const app = await NestFactory.create(AppModule)
+    app.enableCors()
 
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+    app.use(bodyParser.json({ limit: '50mb' }))
+    app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 
-  app.use(bodyParser.json({limit: '50mb'}));
-  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-
-  const port = process.env.PORT || 3000
-  console.log(`Server start at port ${port}`)
-  await app.listen(port);
+    const port = process.env.PORT || 3000
+    console.log(`Server start at port ${port}`)
+    await app.listen(port)
 }
-bootstrap();
+bootstrap()
